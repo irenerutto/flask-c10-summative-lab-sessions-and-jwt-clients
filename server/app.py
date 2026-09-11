ff4a4257c38c181a018b7345d624f3780716a3ee
IRENE <irenerutto@gmail.com>
2026-09-11 23:36:22 +0300

    chore: support production database URL

--- server/app.py
+++ server/app.py
@@ -9,7 +9,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL",
+    "sqlite:///app.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.environ.get(
     "SECRET_KEY",
